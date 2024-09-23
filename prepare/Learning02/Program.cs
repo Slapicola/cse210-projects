df8407a35001fe15d8f6c4ae5f5a5cb6866014b2
Slapicola <lapicolasophia05@gmail.com>
2024-09-23 17:23:12 -0400

    Wrote code for Abstraction Learning Activity 02 (Resume)

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2009;
+        job1._endYear = 2015;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2014;
+        job2._endYear = 2020;
+
+        //job1.Display();
+        //job2.Display();
+
+        Resume resume = new Resume();
+        resume._name = "Fred Turner";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
