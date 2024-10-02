8553af0ae3cd992951fefb1d6f63475c471441f2
Slapicola <lapicolasophia05@gmail.com>
2024-10-02 18:17:48 -0400

    Wrote stub code for Scripture Memorizer program

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,29 @@
+public class Scripture
+{
+    Reference _reference;
+    public List<Words> _scripture = new List<Words>();
+
+
+    public Scripture(Reference reference, string text)
+    {
+
+    }
+
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return true;
+
+    }
+}
---
+++ prove/Develop03/Words.cs
@@ -0,0 +1,33 @@
+public class Words
+{
+    string _text;
+    bool isHidden;
+
+
+    public Words(string text)
+    {
+
+    }
+
+
+    public void Hide()
+    {
+
+    }
+    
+    public void Show()
+    {
+
+    }
+    public bool IsHidden()
+    {
+        return false;
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+
+    }
+}
